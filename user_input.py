5608bbd235b56454a742f2d033ed966d9debdb16
Mosa Mokhaneli <dikeledi.mokhaneli@gmail.com>
2024-08-25 00:15:23 +0200

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Asking the user for their name, age, and location
+name = "Mosa"
+age = "30"
+location = "Johannesburg"
+
+# Printing out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
